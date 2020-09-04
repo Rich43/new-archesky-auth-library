fe2103bc6031728c957b9ddcdd1095ca3e61fefe
Richie Ward <RichieS@GMail.com>
2020-09-04 21:40:04 +0100

    Try fixing ssl error another way

--- src/main/kotlin/com/archesky/auth/library/service/TokenService.kt
+++ src/main/kotlin/com/archesky/auth/library/service/TokenService.kt
@@ -6,35 +6,17 @@ import com.apollographql.apollo.api.Response
 import com.apollographql.apollo.exception.ApolloException
 import com.archesky.auth.library.graphql.CheckTokenQuery
 import com.archesky.auth.library.graphql.CheckTokenQuery.Data
-import com.archesky.auth.library.security.NullHostNameVerifier
-import com.archesky.auth.library.security.NullTrustManager
 import graphql.GraphQLException
-import okhttp3.OkHttpClient
 import org.springframework.core.env.Environment
 import org.springframework.stereotype.Service
-import java.security.SecureRandom
 import java.util.concurrent.CountDownLatch
-import javax.net.ssl.HttpsURLConnection
-import javax.net.ssl.SSLContext
-import javax.net.ssl.SSLSocketFactory
 
 @Service
 class TokenService(private val env: Environment) {
-    private fun configureSSL(): SSLSocketFactory {
-        HttpsURLConnection.setDefaultHostnameVerifier(NullHostNameVerifier())
-        val context: SSLContext = SSLContext.getInstance("TLS")
-        context.init(null, Array(1) { NullTrustManager() }, SecureRandom())
-        return requireNotNull(context.socketFactory, { "socket factory is null" })
-    }
-
     fun validateToken(token: String, serverUrl: String): CheckTokenQuery.CheckToken {
-        val httpClient = OkHttpClient
-                .Builder()
-                .sslSocketFactory(configureSSL(), NullTrustManager())
-                .build()
         val apolloClient = ApolloClient.builder()
                 .serverUrl(serverUrl)
-                .okHttpClient(httpClient)
+                .okHttpClient(OkHttpService.unsafeOkHttpClient)
                 .build()
         val resultLatch = CountDownLatch(1)
         var responseData: Response<Data>? = null
